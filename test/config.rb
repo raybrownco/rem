70bbefb81ed04ed1b0809b563a21e9f736391abb
Adam Stacoviak <adam@stacoviak.com>
2011-08-10 09:41:56 -0500

    Added comments to the Compass config file

--- test/config.rb
+++ test/config.rb
@@ -1,3 +1,6 @@
+# Compass config file
+# To install compass gem install compass (you may be required to use the 'sudo' command to install gems)
+
 http_path       = "/"
 css_dir         = "css"
 sass_dir        = "sass"
